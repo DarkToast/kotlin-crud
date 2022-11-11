0efdf5f5ea192817c4f37dfac182f621597042d3
Christian Schmidt <c.schmidt@tarent.de>
2022-11-11 09:04:25 +0100

    Adding update device spec stubs

--- src/test/kotlin/de/tarent/crud/tests/device/UpdateDeviceSpec.kt
+++ src/test/kotlin/de/tarent/crud/tests/device/UpdateDeviceSpec.kt
@@ -1,4 +1,22 @@
 package de.tarent.crud.tests.device
 
-@Suppress("unused")
-class UpdateDeviceSpec : BaseDeviceSpec()
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+@Disabled
+class UpdateDeviceSpec : BaseDeviceSpec() {
+    @Test
+    fun `update existing device`(): Unit = TODO()
+
+    @Test
+    fun `Renaming existing device`(): Unit = TODO()
+
+    @Test
+    fun `Renaming conflicts`(): Unit = TODO()
+
+    @Test
+    fun `device does not exists`(): Unit = TODO()
+
+    @Test
+    fun `group does not exists`(): Unit = TODO()
+}
