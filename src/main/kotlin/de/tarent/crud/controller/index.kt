f42c2f42e4c9fc8dac124882fb13d3b1b175c3c5
Christian Schmidt <c.schmidt@tarent.de>
2022-11-18 10:43:19 +0100

    Using Index directly

--- src/main/kotlin/de/tarent/crud/controller/index.kt
+++ src/main/kotlin/de/tarent/crud/controller/index.kt
@@ -1,21 +1,14 @@
 package de.tarent.crud.controller
 
 import de.tarent.crud.dtos.Index
-import de.tarent.crud.dtos.Method
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
-import java.net.URI
 
 fun Route.indexPage() {
-    val body = Index()
-        .addLink("_self", Method.GET, URI("/"))
-        .addLink("get_groups", Method.GET, URI("/groups"))
-        .addLink("add_group", Method.POST, URI("/groups"))
-
     get("/") {
-        call.respond(HttpStatusCode.OK, body)
+        call.respond(HttpStatusCode.OK, Index())
     }
 }
