9e251f2ab8f56c12f65c5cf180a015fead8c745a
Christian Schmidt <c.schmidt@tarent.de>
2022-11-22 16:51:30 +0100

    Adding logging to the parameter extensions

--- src/main/kotlin/de/tarent/crud/controller/parameter.kt
+++ src/main/kotlin/de/tarent/crud/controller/parameter.kt
@@ -8,22 +8,36 @@ import io.ktor.server.request.httpMethod
 import io.ktor.server.request.path
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
+import mu.KotlinLogging
 
 suspend fun ApplicationCall.path(parameterName: String): String? {
     val parameter = this.parameters[parameterName]
-    if(parameter == null) {
+
+    if (parameter == null) {
+        val msg = "Parameter '$parameterName' not found"
+
+        val logger = KotlinLogging.logger {}
+        logger.warn(msg)
+
         this.respond(BadRequest, Failure(400, "Parameter '$parameterName' not found"))
     }
+
     return parameter
 }
 
 suspend inline fun <reified T : Any> ApplicationCall.body(
     failure: (msg: String) -> Failure = { Failure.onIndex(400, it) }
-): T? = try {
-    this.receive()
-} catch (e: BadRequestException) {
-    val req = "${this.request.httpMethod} ${this.request.path()}"
-    val msg = "Bad request body on call '$req'!"
-    this.respond(BadRequest, failure(msg))
-    null
+): T? {
+    return try {
+        this.receive()
+    } catch (e: BadRequestException) {
+        val req = "${this.request.httpMethod} ${this.request.path()}"
+        val msg = "Bad request body on call '$req'!"
+
+        val logger = KotlinLogging.logger {}
+        logger.warn(e) { msg }
+
+        this.respond(BadRequest, failure(msg))
+        null
+    }
 }
