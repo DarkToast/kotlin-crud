0f9a38739e8b1cab77e3fdb4134153cc01fc31b2
Christian Schmidt <c.schmidt@tarent.de>
2022-11-22 17:00:46 +0100

    Adding metric entity

--- src/main/kotlin/de/tarent/crud/persistance/Entities.kt
+++ src/main/kotlin/de/tarent/crud/persistance/Entities.kt
@@ -1,6 +1,8 @@
 package de.tarent.crud.persistance
 
+import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.datetime
 
 object GroupEntity : Table("group") {
     val name = varchar("name", 50)
@@ -16,4 +18,16 @@ object DeviceEntity : Table("device") {
     val groupId = varchar("group_id", 50) references GroupEntity.name
 
     override val primaryKey = PrimaryKey(arrayOf(name, groupId), name = "device_pk")
+}
+
+@Suppress("unused")
+object MetricEntity : UUIDTable("metric") {
+    val unit = varchar("unit", 8)
+    val value = decimal("value", 10, 2)
+    val timestamp = datetime("timestamp")
+    val deviceId = varchar("device_id", 50) references DeviceEntity.name
+
+    init {
+        uniqueIndex(timestamp, unit, deviceId)
+    }
 }
