4143d745cf7d221c8127a4d74eb43b64d0b2cad7
Christian Schmidt <c.schmidt@tarent.de>
2022-11-23 09:22:48 +0100

    Using the metricService in the metrics route

--- src/main/kotlin/de/tarent/crud/controller/metrics.kt
+++ src/main/kotlin/de/tarent/crud/controller/metrics.kt
@@ -1,6 +1,10 @@
 package de.tarent.crud.controller
 
 import de.tarent.crud.dtos.Metric
+import de.tarent.crud.service.MetricService
+import de.tarent.crud.service.results.DeviceDontExists
+import de.tarent.crud.service.results.GroupDontExists
+import de.tarent.crud.service.results.Ok
 import io.ktor.http.HttpStatusCode.Companion.Created
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
@@ -9,7 +13,7 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import mu.KotlinLogging
 
-fun Route.metricsPage() {
+fun Route.metricsPage(metricService: MetricService) {
     val logger = KotlinLogging.logger {}
 
     route("/groups/{groupName?}/devices/{deviceName?}/metrics") {
@@ -17,9 +21,13 @@ fun Route.metricsPage() {
             val groupName = call.path("groupName") ?: return@post
             val deviceName = call.path("deviceName") ?: return@post
             val metric: Metric = call.body() ?: return@post
-
             logger.info { "POST new metric on device '$deviceName' of group '$groupName'" }
-            call.respond(Created, metric.withLinks(groupName, deviceName))
+
+            when (val result = metricService.create(groupName, deviceName, metric)) {
+                is GroupDontExists -> groupDontExists(call, result)
+                is DeviceDontExists -> deviceDontExist(call, result)
+                is Ok -> call.respond(Created, metric.withLinks(groupName, deviceName))
+            }
         }
     }
 
