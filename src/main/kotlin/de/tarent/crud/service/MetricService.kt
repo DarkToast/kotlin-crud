83089d912abe3ad4e048d41edd6769cabfa478b9
Christian Schmidt <c.schmidt@tarent.de>
2022-11-24 13:55:10 +0100

    Adding delete service layer

--- src/main/kotlin/de/tarent/crud/service/MetricService.kt
+++ src/main/kotlin/de/tarent/crud/service/MetricService.kt
@@ -1,5 +1,6 @@
 package de.tarent.crud.service
 
+import de.tarent.crud.dtos.Device
 import de.tarent.crud.dtos.Metric
 import de.tarent.crud.persistance.DeviceRepository
 import de.tarent.crud.persistance.GroupRepository
@@ -7,6 +8,7 @@ import de.tarent.crud.persistance.MetricRepository
 import de.tarent.crud.service.results.DeviceDontExists
 import de.tarent.crud.service.results.GroupDontExists
 import de.tarent.crud.service.results.MetricCreateResult
+import de.tarent.crud.service.results.MetricDeleteResult
 import de.tarent.crud.service.results.MetricDontNotExists
 import de.tarent.crud.service.results.MetricReadResult
 import de.tarent.crud.service.results.Ok
@@ -42,5 +44,23 @@ class MetricService(
         return metricRepository.load(metricId)
             ?.let { Ok(it) }
             ?: MetricDontNotExists(groupName, deviceName, metricId)
+    }
+
+    fun delete(groupName: String, deviceName: String, metricId: UUID): MetricDeleteResult<Device> {
+        if (!groupRepository.exists(groupName)) {
+            return GroupDontExists(groupName)
+        }
+
+        if (!deviceRepository.exists(groupName, deviceName)) {
+            return DeviceDontExists(groupName, deviceName)
+        }
+
+        return if (metricRepository.delete(metricId) == 1) {
+            deviceRepository.load(groupName, deviceName)
+                ?.let { Ok(it) }
+                ?: GroupDontExists(groupName)
+        } else {
+            MetricDontNotExists(groupName, deviceName, metricId)
+        }
     }
 }
