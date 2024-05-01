4bac0c72159de29e35d02da4a79c5e5abad0d65c
Christian Schmidt <christian.iwanzik@gmail.com>
2024-05-01 20:38:42 +0200

    Using kotlin gradle. Adding dependency lib. Upgrade gradle

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    application
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.versions)
+}
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "de.tarent.crud.ApplicationKt"
+}
+
+dependencies {
+    implementation(libs.web.ktor.server.core)
+    implementation(libs.web.ktor.server.netty)
+    implementation(libs.web.ktor.server.pages)
+    implementation(libs.web.ktor.server.content.negotiation)
+    implementation(libs.web.ktor.serialization.json)
+
+    implementation(libs.di.koin.core)
+    implementation(libs.di.koin.ktor)
+    implementation(libs.di.koin.logger)
+
+    implementation(libs.db.exposed.core)
+    implementation(libs.db.exposed.dao)
+    implementation(libs.db.exposed.jdbc)
+    implementation(libs.db.exposed.time)
+
+    implementation(libs.db.hikari)
+    runtimeOnly(libs.db.postgres.driver)
+
+    implementation(libs.logging.logback)
+    implementation(libs.logging.kotlin)
+
+    testImplementation(libs.test.junit.engine)
+    testImplementation(libs.test.junit.params)
+    testImplementation(libs.test.assertj)
+    testImplementation(libs.test.ktor.server)
+    testImplementation(libs.test.koin)
+
+    testRuntimeOnly(libs.test.h2)
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging.showStandardStreams = true
+}
+
+//tasks.register("prepareImage", Copy) {
+//    from "$buildDir/resources/main/Dockerfile"
+//    into "$buildDir/install"
+//}
